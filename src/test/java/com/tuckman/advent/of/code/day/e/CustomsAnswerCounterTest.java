e2a833fe60d876413142ac535b0af966edbe38ec
Matt Tuckman <matttuckman@Matts-MacBook-Pro.local>
2020-12-06 20:11:20 -0500

    Day6

---
+++ src/test/java/com/tuckman/advent/of/code/day/e/CustomsAnswerCounterTest.java
@@ -0,0 +1,47 @@
+package com.tuckman.advent.of.code.day.e;
+
+import com.tuckman.advent.of.code.commons.AdventUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomsAnswerCounterTest {
+    CustomsAnswerCounter customsAnswerCounter = new CustomsAnswerCounter();
+
+
+    @Test
+    void test_acutalInput() {
+        List<String> input = AdventUtils.listLinesOfResource("customsDeclarations.txt");
+        int numAnswers = customsAnswerCounter.countTotalAnswers(input);
+        System.out.println("Total actual answers: " + numAnswers);
+
+    }
+
+    @Test
+    void test_exampleInput() {
+        List<String> input = AdventUtils.listLinesOfResource("exampleCustomsDeclarations.txt");
+        int numAnswers = customsAnswerCounter.countTotalAnswers(input);
+        System.out.println("Total example answers: " + numAnswers);
+
+        assertEquals(11, numAnswers);
+    }
+
+    @Test
+    void test_acutalInput_2() {
+        List<String> input = AdventUtils.listLinesOfResource("customsDeclarations.txt");
+        int numAnswers = customsAnswerCounter.countTotalAnswers2(input);
+        System.out.println("Total actual answers: " + numAnswers);
+
+    }
+
+    @Test
+    void test_exampleInput_2() {
+        List<String> input = AdventUtils.listLinesOfResource("exampleCustomsDeclarations.txt");
+        int numAnswers = customsAnswerCounter.countTotalAnswers2(input);
+        System.out.println("Total example answers: " + numAnswers);
+
+        assertEquals(6, numAnswers);
+    }
+}
