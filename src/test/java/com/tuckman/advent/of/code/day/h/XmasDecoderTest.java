ec35a63aaeb72e6bb65e870e1b452f0ea3f24862
Matt Tuckman <mrtuckman@gmail.com>
2020-12-09 00:52:33 -0500

    Day 9, realizing people code quickly

---
+++ src/test/java/com/tuckman/advent/of/code/day/h/XmasDecoderTest.java
@@ -0,0 +1,44 @@
+package com.tuckman.advent.of.code.day.h;
+
+import com.tuckman.advent.of.code.commons.AdventUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class XmasDecoderTest {
+    XmasDecoder xmasDecoder = new XmasDecoder();
+
+    @Test
+    void test_exampleInput() {
+        List<String> exampleInput = AdventUtils.listLinesOfResource("exampleXmasCode.txt");
+        long invalidNum = xmasDecoder.findFirstInvalidNumber(exampleInput, 5);
+        System.out.println("Example invalid num: " + invalidNum);
+        assertEquals(127L, invalidNum);
+    }
+
+    @Test
+    void test_actualInput() {
+        List<String> actualInput = AdventUtils.listLinesOfResource("xmasCode.txt");
+        long invalidNum = xmasDecoder.findFirstInvalidNumber(actualInput, 25);
+        System.out.println("Actual invalid num: " + invalidNum);
+        assertEquals(1038347917L, invalidNum);
+    }
+
+    @Test
+    void test_exampleInput_weakness() {
+        List<String> exampleInput = AdventUtils.listLinesOfResource("exampleXmasCode.txt");
+        long weakness = xmasDecoder.findContinuousSum(exampleInput, 5, 127L);
+        System.out.println("Example invalid num: " + weakness);
+        assertEquals(62L, weakness);
+    }
+
+    @Test
+    void test_actualInput_weakness() {
+        List<String> actualInput = AdventUtils.listLinesOfResource("xmasCode.txt");
+        long weakness = xmasDecoder.findContinuousSum(actualInput, 25, 1038347917L);
+        System.out.println("Actual invalid num: " + weakness);
+        assertEquals(137394018L, weakness);
+    }
+}
---
+++ src/test/java/com/tuckman/advent/of/code/day/template/TemplateTest.java
@@ -0,0 +1,19 @@
+package com.tuckman.advent.of.code.day.template;
+
+import com.tuckman.advent.of.code.commons.AdventUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TemplateTest {
+
+    @Test
+    void test_exampleInput() {
+        List<String> exampleInput = AdventUtils.listLinesOfResource("");
+    }
+
+    @Test
+    void test_actualInput() {
+        List<String> actualInput = AdventUtils.listLinesOfResource("");
+    }
+}
