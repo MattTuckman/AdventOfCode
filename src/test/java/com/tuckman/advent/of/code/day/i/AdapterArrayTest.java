3fd479301fd6b3573d83d8a5241fdde097c73629
Matt Tuckman <mrtuckman@gmail.com>
2020-12-10 01:20:48 -0500

    Day 10, fun problem. Had to think

---
+++ src/test/java/com/tuckman/advent/of/code/day/i/AdapterArrayTest.java
@@ -0,0 +1,76 @@
+package com.tuckman.advent.of.code.day.i;
+
+import com.tuckman.advent.of.code.commons.AdventUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdapterArrayTest {
+
+    AdapterArray adapterArray = new AdapterArray();
+    @Test
+    void test_exampleInput1_differences() {
+        Stream<String> exampleInput = AdventUtils.streamLinesOfResource("exampleAdapter.txt");
+        long diffs = adapterArray.numberOfDifferences(exampleInput);
+        System.out.println("Example 1 diffs: " + diffs);
+        assertEquals(35, diffs);
+    }
+
+    @Test
+    void test_exampleInput2_differences() {
+        Stream<String> exampleInput = AdventUtils.streamLinesOfResource("exampleAdapter2.txt");
+        long diffs = adapterArray.numberOfDifferences(exampleInput);
+        System.out.println("Example 2 diffs: " + diffs);
+        assertEquals(220, diffs);
+    }
+
+    @Test
+    void test_actualInput_differences() {
+        Stream<String> actualInput = AdventUtils.streamLinesOfResource("adapter.txt");
+        long diffs = adapterArray.numberOfDifferences(actualInput);
+        System.out.println("Actual diffs: " + diffs);
+        assertEquals(2059, diffs);
+    }
+
+    @Test
+    void test_exampleInput1_arrangements_fullRecursion() {
+        Stream<String> exampleInput = AdventUtils.streamLinesOfResource("exampleAdapter.txt");
+        long diffs = adapterArray.numberOfArrangementsFullyRecursive(exampleInput);
+        System.out.println("Example 1 arrangements: " + diffs);
+        assertEquals(8, diffs);
+    }
+
+    @Test
+    void test_exampleInput2_arrangements_fullRecursion() {
+        Stream<String> exampleInput = AdventUtils.streamLinesOfResource("exampleAdapter2.txt");
+        long diffs = adapterArray.numberOfArrangementsFullyRecursive(exampleInput);
+        System.out.println("Example 2 arrangements: " + diffs);
+        assertEquals(19208, diffs);
+    }
+
+    @Test
+    void test_exampleInput1_arrangements() {
+        Stream<String> exampleInput = AdventUtils.streamLinesOfResource("exampleAdapter.txt");
+        long diffs = adapterArray.numberOfArrangements(exampleInput);
+        System.out.println("Example 1 arrangements: " + diffs);
+        assertEquals(8, diffs);
+    }
+
+    @Test
+    void test_exampleInput2_arrangements() {
+        Stream<String> exampleInput = AdventUtils.streamLinesOfResource("exampleAdapter2.txt");
+        long diffs = adapterArray.numberOfArrangements(exampleInput);
+        System.out.println("Example 2 arrangements: " + diffs);
+        assertEquals(19208, diffs);
+    }
+
+    @Test
+    void test_actualInput_arrangements() {
+        Stream<String> actualInput = AdventUtils.streamLinesOfResource("adapter.txt");
+        long diffs = adapterArray.numberOfArrangements(actualInput);
+        System.out.println("Actual arrangements: " + diffs);
+        assertEquals(86812553324672L, diffs);
+    }
+}
